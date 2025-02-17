d5cd95fe55f3c11a7c64310806bc5853ba466b9e
anaperez <ana.perez.carrillo77@gmail.com>
2025-02-17 10:05:06 -0600

    Nuevos archivos

---
+++ Driver.java
@@ -0,0 +1,16 @@
+class Driver extends Account {
+
+    public Driver(String name, String
+    document, String email, String email, String
+    password){
+        super(name, document, email, 
+        password);
+    }
+    void printDataDriver(){
+        System.out.println("Document
+        driver: " + document + " Name
+        driver: " + name + "E-mail"
+        + email + " Password: "+
+        password);
+    }
+}
---
+++ User.java
@@ -0,0 +1,15 @@
+class User extends Acoount{
+    public User(String name, String
+    socument, String email, Straing
+    password){
+        super(name,document,email,password);
+    }
+
+void printDataUser(){
+    System.out.println("Document
+    driver:" + document + "Name
+    driver: " + name + " E-mail: "
+    + email + " Password: "+
+    password);
+}
+}
